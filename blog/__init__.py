877698cedcfebdb3acbc31afb7aef8d2403e4daa
Nicholas Studt <nicholas@nicholasstudt.com>
2009-09-09 03:30:12 +0000

    Fixing version numbers so they make sense.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,8 @@
-VERSION = (1, 0, 1, 'rc', 1)
+VERSION = (1, 0, 0, 'final', 20100908)
+#          0  1  2  3        4
 
 def get_version():
-    version = '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3]) 
+    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2]) 
     
     if VERSION[3] != 'final':
         version = '%s %s' % (version, VERSION[4])
